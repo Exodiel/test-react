4a86daa2905a4dc8124ffcbd1a69b3b42204edda
Jipson <jipson09saad@gmail.com>
2022-03-06 20:27:41 -0500

    init repo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen, waitFor } from '@testing-library/react'
+import { mocked } from 'ts-jest/utils'
+import App from './App'
+import { getUser } from './get-user'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+jest.mock('./get-user')
+const mockedGetUser = mocked(getUser, true)
+
+describe('When everything is OK', () => {
+  beforeEach(async () => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    render(<App />)
+    await waitFor(() => expect(mockedGetUser).toHaveBeenCalled())
+  })
+  
+  test('should select the children that is being passed to the CustomInput component', () => {
+    expect(screen.getByText(/Input/)).toBeInTheDocument()
+  })
+
+  test('should select the input element by its role', () => {
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
+  })
+
+  test('should select a label element by its text', () => {
+    expect(screen.getByLabelText('Input:')).toBeInTheDocument()
+  })
+
+  test('should select input element by placeholder text', () => {
+    expect(screen.getByPlaceholderText('Example')).toBeInTheDocument()
+  })
+
+  test('should select the input element by its role by queryByRole', () => {
+    const result = screen.queryByRole('textbox')
+    expect(result).toBeInTheDocument()
+  })
+})
+
+describe('When the component fetches the user successfully', () => {
+  beforeEach(() => {
+    mockedGetUser.mockClear()
+  })
+
+  test('should call getUser Once', async () => {
+    render(<App />)
+    await waitFor(() => expect(mockedGetUser).toHaveBeenCalledTimes(1))
+  })
+
+  test('should render the username passed', async () => {
+    const name = 'Pepito'
+    mockedGetUser.mockResolvedValueOnce({id: "1", name})
+    render(<App />)
+    expect(screen.queryByText(/Username/)).toBeNull()
+    expect(await screen.findByText(/Username/)).toBeInTheDocument()
+    expect(await screen.findByText(`Username: ${name}`)).toBeInTheDocument()
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { getUser, User } from './get-user'
 
 function App() {
+  const [text, setText] = React.useState('')
+  const [user, setUser] = React.useState<User | null>(null)
+
+  React.useEffect(() => {
+    const fetchUser = async () => {
+      const user = await getUser()
+      setUser(user)
+    }
+    fetchUser()
+  }, [])
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setText(event.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? <p>Username: {user.name}</p> : null}
+      <CustomInput value={text} onChange={handleChange}>
+        Input:
+      </CustomInput>
+      <p>You typed: {text ? text : '...'}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+interface CustomInputProps {
+  children: React.ReactNode
+  value: string
+  onChange(event: React.ChangeEvent<HTMLInputElement>): void
+}
+
+function CustomInput({ children, value, onChange }: CustomInputProps) {
+  return (
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input
+        id="search"
+        type="text"
+        placeholder="Example"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  )
+}
+
+export default App
